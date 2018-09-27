004f872ef4ee9b60c7962e3a6cac02fd52f59cae
unknown <churui@ctecdcs.net>
2018-09-27 16:19:05 +0800

    second commit

--- load_source.py
+++ load_source.py
@@ -1,10 +1,25 @@
 import os
+# import collections
 from openpyxl import load_workbook
 
 files = os.listdir('./source')
+col_keyword = '功能描述'
+# Cell = collections.namedtuple('Cell', ['ID', 'description'])
 
-signal_pool = []
+pool = []
 for file in files:
     wb = load_workbook(os.path.join(os.getcwd(), 'source', file)) 
     ws = wb.active
-    print(ws.title)
+    # id_des = [(row[3].value, [row[3].value]) for row in ws]
+    id_des = []
+    double_description_col = col_keyword in ws['D1'].value
+    for row in ws.rows:
+        # print(row)
+        id = row[0].value
+        description = [row[2].value]
+        if double_description_col:
+            description.append(row[3].value)
+        id_des.append((id, description))
+    pool.extend(id_des)    
+
+print(pool)
